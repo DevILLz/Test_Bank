c05b4ffba09ef10592a2c7261c9f728eaac11f82
DevILLz <olddevillz@gmail.com>
2021-09-01 14:27:56 +0300

    Добавлены скрипты для создания таблиц

---
+++ dbo.Clients.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Clients] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Type]        NCHAR (7)      NOT NULL,
+    [FullName]    NVARCHAR (80)  NOT NULL,
+    [MainAccount] MONEY          NOT NULL,
+    [Address]     NVARCHAR (100) NOT NULL,
+    [BankAccount] MONEY          NOT NULL,
+    [Reliability] BIT            NOT NULL,
+    [Credit]      MONEY          NOT NULL,
+    [PhoneNumber] NCHAR (14)     NOT NULL,
+    [Current]     TINYINT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo.Logs.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Logs] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Date]        DATETIME2 (2) NOT NULL,
+    [MoneyAmount] MONEY         NOT NULL,
+    [SenderID]    INT           NOT NULL,
+    [RecipientID] INT           NOT NULL,
+    [Successful]  BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
